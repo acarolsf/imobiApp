d624bf777675bd83315655b8d5d94854632f914a
Ana Carolina <cscarol20@gmail.com>
2018-06-10 23:52:12 -0300

    add the splash

---
+++ app/src/main/java/br/com/abjdesenvolvimentos/imobiapp/SplashActivity.java
@@ -0,0 +1,26 @@
+package br.com.abjdesenvolvimentos.imobiapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity implements Runnable {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler h = new Handler();
+        h.postDelayed(this, 3000);
+    }
+
+    @Override
+    public void run() {
+        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
+
